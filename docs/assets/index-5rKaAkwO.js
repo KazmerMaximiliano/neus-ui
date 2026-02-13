725d3621ddbd22c72817cc5f6b5d7f2abb91cc9c
KazmerMaximiliano <kazmermaximiliano@gmail.com>
2026-02-13 16:49:36 -0300

    feat: update pagination controls in DataTable for improved alignment and styling

--- docs/assets/index-5rKaAkwO.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-BzhZkEaw.js";import{r as o}from"./index-lbPttQvA.js";var t=o();const m=r(t);export{m as R,t as r};
---
+++ docs/assets/index-6RWmgpQt.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-CQynyMMU.js";import{r as o}from"./index-5uY27oyS.js";var t=o();const m=r(t);export{m as R,t as r};
